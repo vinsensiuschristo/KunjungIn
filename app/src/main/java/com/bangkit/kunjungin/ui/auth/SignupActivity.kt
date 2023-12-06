a6e7204170b95a3d41388903c3abfdc1d8bb59f7
Muhamad Ivan Fadillah <70643310+ivantendou@users.noreply.github.com>
2023-12-06 14:08:58 +0700

    Update and rename SIgnupActivity.kt to SignupActivity.kt

--- app/src/main/java/com/bangkit/kunjungin/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/bangkit/kunjungin/ui/auth/SignupActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.kunjungin.databinding.ActivitySignupBinding
 
-class SIgnupActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
 
