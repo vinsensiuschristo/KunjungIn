d4f911799ea14fc4987dbf79c7bc413a25f4d1f9
ivantendou <ivanfadillah153@gmail.com>
2023-12-06 14:10:00 +0700

    add spannable string for link between signup and login activity

--- app/src/main/java/com/bangkit/kunjungin/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/bangkit/kunjungin/ui/auth/LoginActivity.kt
@@ -1,7 +1,15 @@
 package com.bangkit.kunjungin.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.kunjungin.R
 import com.bangkit.kunjungin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +20,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val signupTextView: TextView = binding.toSignup
+        val spannableString = SpannableString(getString(R.string.to_signup))
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                val intent = Intent(this@LoginActivity, SignupActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(
+            clickableSpan, 18, spannableString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        signupTextView.text = spannableString
+        signupTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
--- app/src/main/java/com/bangkit/kunjungin/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/bangkit/kunjungin/ui/auth/SignupActivity.kt
@@ -1,7 +1,15 @@
 package com.bangkit.kunjungin.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.kunjungin.R
 import com.bangkit.kunjungin.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
@@ -12,5 +20,19 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val signupTextView: TextView = binding.toLogin
+        val spannableString = SpannableString(getString(R.string.to_login))
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(
+            clickableSpan, 18, spannableString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        signupTextView.text = spannableString
+        signupTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
